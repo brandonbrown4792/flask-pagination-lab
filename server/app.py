22dff4773a37abb9b0615dce0d5f7768a617e62b
Brandon Brown <brandonbrown4792@gmail.com>
2025-06-22 22:46:12 -0400

    Update routes

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@
 from flask import request, session, jsonify, make_response
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
+from flask_restful import Api
 
 import os
 from config import create_app, db, api
@@ -10,14 +11,31 @@ from models import Book, BookSchema
 
 env = os.getenv("FLASK_ENV", "dev")
 app = create_app(env)
+api = Api(app)
+
+
+class Index(Resource):
+    def get(self):
+        return { 'message': 'Hi there' }, 200
+
 
 class Books(Resource):
     def get(self):
-        books = [BookSchema().dump(b) for b in Book.query.all()]
-        return books, 200
+        page = request.args.get("page", 1, type=int)
+        per_page = request.args.get("per_page", 5, type=int)
+        pagination = Book.query.paginate(page=page, per_page=per_page, error_out=False)
+        books = pagination.items
+        return {
+            "page": page,
+            "per_page": per_page,
+            "total": pagination.total,
+            "total_pages": pagination.pages,
+            "items": [BookSchema().dump(book) for book in books]
+        }, 200
 
 
 api.add_resource(Books, '/books', endpoint='books')
+api.add_resource(Index, '/', endpoint='index')
 
 
 if __name__ == '__main__':
